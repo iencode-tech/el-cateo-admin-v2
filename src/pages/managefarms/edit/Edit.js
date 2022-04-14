b9db5cba338f2ceeb43f4e02d5feacf1dceda01e
Rituparna Shome <rituparna.iencodetech@gmail.com>
2022-04-14 19:04:01 +0530

    still working on image update

--- src/pages/managefarms/edit/Edit.js
+++ src/pages/managefarms/edit/Edit.js
@@ -3,19 +3,27 @@ import { useHistory } from 'react-router-dom';
 import Breadcrumb from "../../../components/common/breadcrumb/Breadcrumb";
 import axios from "axios";
 import { useParams } from "react-router";
+import { statuses } from "../../../utils/appConstants";
 
 
 function FarmEdit() {
-    const [name, setName] = useState("");
-    const [nameError, setNameError] = useState("");
 
-    const [address, setAddress] = useState("");
-    const [addressError, setaddressError] = useState("");
+    const initialValues = {
+        name: "",
+        assignedOwner: "",
+        address: "",
+        area: "",
+        status: "",
+        files: ""
+    };
 
-    const [area, setArea] = useState("");
-    const [areaError, setAreaError] = useState("");
-
-    const [status, setStatus] = useState(1);
+    const history = useHistory();
+    const params = useParams();
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [image, setImage] = useState(null);
+    const [imageData, setImageData] = useState(null);
 
     const pageName = "Edit Farm Data";
     const breadCrumbs = [
@@ -32,33 +40,52 @@ function FarmEdit() {
             link: "",
         },
     ];
-    const history = useHistory();
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            setImage(URL.createObjectURL(event.target.files[0]));
+            setImageData(event.target.files[0]);
+        }
+    };
 
-    const params = useParams();
+    const _handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
 
     //api calling for get farm by id
     let getFarmData = async () => {
-        await axios.get(`${process.env.REACT_APP_API_URL}/farm/${params.id}/read`, { headers: { "authorization": localStorage.getItem(process.env.REACT_APP_AUTH_KEY_NAME) } }).then((response) => {
-            const userData = response.data.data;
-            setArea(userData.area);
-            setAddress(userData.address);
-            setStatus(userData.status);
-            setName(userData.name)
+        await axios.get(`${process.env.REACT_APP_API_URL}/farm/${params.id}/read`,
+            {
+                headers: { "authorization": localStorage.getItem(process.env.REACT_APP_AUTH_KEY_NAME) }
+            }).then((response) => {
+                setImage(response.data.data.files[0])
+                setFormValues(response.data.data);
 
-        }).catch(error => {
-            console.log(error.response)
-        });
+            }).catch(error => {
+                console.log(error.response)
+            });
 
     };
 
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+        }
+    }, [formErrors]);
+
     // api calling for update user
-    const createFarm = async () => {
-        await axios.put(`${process.env.REACT_APP_API_URL}/farm/${params.id}/update`, {
-            "name": name,
-            "address": address,
-            "area": area,
-            "status": status
-        },
+    const _handleFormSubmit = async (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        let formData = new FormData();
+        formData.append("name", formValues.name);
+        formData.append("assignedOwner", formValues.assignedOwner);
+        formData.append("address", formValues.address);
+        formData.append("area", formValues.area);
+        formData.append("status", formValues.status);
+        formData.append("files", imageData);
+        setIsSubmit(true);
+        await axios.put(`${process.env.REACT_APP_API_URL}/farm/${params.id}/update`, formData,
             {
                 headers: {
                     'authorization': localStorage.getItem(process.env.REACT_APP_AUTH_KEY_NAME),
@@ -72,42 +99,34 @@ function FarmEdit() {
     };
 
     // for submit
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        let error = false;
-
-        if (!name) {
-            setNameError("Required");
-            error = true;
-        } else {
-            setNameError("");
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[a-zA-Z]{3,}$/i;
+        const renum = /^[0-9\b]+$/i;
+        if (!values.name) {
+            errors.name = "Name is required!";
+        } else if (!regex.test(values.name)) {
+            errors.name = "This is not a valid name!";
         }
-
-        if (!address) {
-            setaddressError("Required");
-            error = true;
-        } else {
-            setaddressError("");
-
+        if (!values.address) {
+            errors.address = "Address is required!";
         }
-
-        if (!area) {
-            setAreaError("Required");
-            error = true;
-        } else {
-            setAreaError("");
-
+        if (!values.area) {
+            errors.area = "Area is required!";
+        } else if (!renum.test(values.area)) {
+            errors.area = "This is not a valid area format!";
         }
-
-        if (error == false) {
-            createFarm();
-        }
-    }
+        return errors;
+    };
 
     useEffect(() => {
         getFarmData();
     }, [])
-
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+        }
+    }, [formErrors]);
 
     return (
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -134,7 +153,7 @@ function FarmEdit() {
                                         <div className={"row"}>&nbsp;</div>
                                     </div>
 
-                                    <form onSubmit={handleSubmit}
+                                    <form onSubmit={_handleFormSubmit}
                                         encType={`multipart/form-data`}>
                                         <div className="card-body">
                                             <div className="row mb-3">
@@ -146,10 +165,10 @@ function FarmEdit() {
                                                         className="form-control"
                                                         id="name"
                                                         placeholder="Enter name"
-                                                        value={name}
-                                                        onChange={(e) => setName(e.target.value)}
+                                                        value={formValues.name}
+                                                        onChange={_handleOnChange}
                                                     />
-                                                    <p style={{ color: 'red' }}>{nameError}</p>
+                                                    <p style={{ color: 'red' }}>{formErrors.name}</p>
                                                 </div>
 
 
@@ -162,10 +181,10 @@ function FarmEdit() {
                                                         className="form-control"
                                                         id="name"
                                                         placeholder="Enter area"
-                                                        value={area}
-                                                        onChange={(e) => setArea(e.target.value)}
+                                                        value={formValues.area}
+                                                        onChange={_handleOnChange}
                                                     />
-                                                    <p style={{ color: 'red' }}>{areaError}</p>
+                                                    <p style={{ color: 'red' }}>{formErrors.area}</p>
                                                 </div>
 
                                                 <div className="col-md-6">
@@ -177,10 +196,10 @@ function FarmEdit() {
                                                         className="form-control"
                                                         id="location"
                                                         placeholder="Enter address"
-                                                        value={address}
-                                                        onChange={(e) => setAddress(e.target.value)}
+                                                        value={formValues.address}
+                                                        onChange={_handleOnChange}
                                                     />
-                                                    <p style={{ color: 'red' }}>{addressError}</p>
+                                                    <p style={{ color: 'red' }}>{formErrors.address}</p>
                                                 </div>
 
                                                 <div className="col-md-6">
@@ -190,12 +209,34 @@ function FarmEdit() {
                                                         id="input4"
                                                         className="form-select"
                                                         name="status"
-                                                        value={status}
-                                                        onChange={(e) => setStatus(e.target.value)}
+                                                        value={formValues.status}
+                                                        onChange={_handleOnChange}
                                                     >
-                                                        <option value={1}>Active</option>
-                                                        <option value={0}>Inactive</option>
+                                                        {Object.keys(statuses).map((status, index) => (
+                                                            <option key={index} value={status}>
+                                                                {statuses[status]}
+                                                            </option>
+                                                        ))}
                                                     </select>
+                                                </div>
+
+                                                <div className="col-md-6">
+                                                    <div style={{
+                                                        display: 'flex',
+                                                        margin: 10,
+                                                        width: 400,
+                                                        flexWrap: 'wrap',
+                                                    }}>
+                                                        <input accept="image/*" id="icon-button-file"
+                                                            type="file" onChange={onImageChange}
+                                                            value={formValues.files}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <img src={image} width={300}
+                                                        height={150} />
+
                                                 </div>
                                             </div>
                                         </div>
